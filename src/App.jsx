1076d485662fc4e301401b02961faaea4a153930
Martin Beňa <Bena.M@seznam.cz>
2023-10-23 16:24:15 +0200

    Add basic file structure and react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/contact",
+    element: <Menu />,
+  },
+  {
+    path: "/reservation/new",
+    element: <Menu />,
+  },
+  {
+    path: "/reservation/:reservationId",
+    element: <Menu />,
+  },
+]);
+
 function App() {
-  return <div>Hello world!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
