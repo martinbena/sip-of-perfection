747591ef9f2befc04c99e8f4b9e0099938329711
Martin Beňa <Bena.M@seznam.cz>
2023-10-24 13:42:32 +0200

    Create the color palette and set the text color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,20 @@ export default {
       sans: "Open Sans, sans-serif",
       heading: "Roboto Slab, serif",
     },
-    extend: {},
+    extend: {
+      colors: {
+        accent: "#51C0C8",
+        accentshade: "#0E737F",
+        text: "#503000",
+        textbtn: "#222222",
+        brand: "#FFDBA5",
+        brandshade: "#FFE9C8",
+        brandtint: "#FFF4E3",
+        wavetop: "#F5F8F5",
+        wavemid: "#C2AE91",
+        wavebot: "#886C47",
+      },
+    },
   },
   plugins: [],
 };
