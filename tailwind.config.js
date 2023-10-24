cb7c54e544d02cd070df2162a43111a9500acfd4
Martin Beňa <Bena.M@seznam.cz>
2023-10-24 13:04:07 +0200

    Include google fonts and configure fonts in Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: "Open Sans, sans-serif",
+      heading: "Roboto Slab, serif",
+    },
     extend: {},
   },
   plugins: [],
