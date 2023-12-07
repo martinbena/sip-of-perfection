3bcc77b8f093b20d54baeb6120f8a3b7754795d4
Martin Beňa <Bena.M@seznam.cz>
2023-12-07 21:50:28 +0100

    Add search bar to the menu

--- src/features/menu/MenuFunctions.jsx
+++ src/features/menu/MenuFunctions.jsx
@@ -13,47 +13,130 @@ function MenuFunctions({ menu }) {
 
   const ACTIONS = {
     SELECT_SORT_BY: "select-sort-by",
+    SET_FILTER: "set-filter",
+    CHANGE_SEARCH_TERM: "change-search-term",
   };
 
   const initialState = {
     sortBy: "default",
+    filter: "default",
+    searchTerm: "",
   };
 
   function reducer(state, action) {
     switch (action.type) {
       case ACTIONS.SELECT_SORT_BY:
         return { ...state, sortBy: action.payload };
+      case ACTIONS.SET_FILTER:
+        return { ...state, filter: action.payload };
+      case ACTIONS.CHANGE_SEARCH_TERM:
+        return { ...state, searchTerm: action.payload };
       default:
         return state;
     }
   }
 
-  const [{ sortBy }, dispatch] = useReducer(reducer, initialState);
+  const [{ sortBy, filter, searchTerm }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
+
+  const filteredItems =
+    filter === "default"
+      ? menu
+          .slice()
+          .filter((item) =>
+            item.name.toLowerCase().includes(searchTerm.toLowerCase()),
+          )
+      : menu
+          .slice()
+          .filter((item) => item.category === filter)
+          .filter((item) =>
+            item.name.toLowerCase().includes(searchTerm.toLowerCase()),
+          );
 
   let sortedItems;
-  if (sortBy === "default") sortedItems = menu;
+  if (sortBy === "default") sortedItems = filteredItems;
   if (sortBy === "nameAZ")
-    sortedItems = menu.slice().sort((a, b) => a.name.localeCompare(b.name));
+    sortedItems = filteredItems
+      .slice()
+      .sort((a, b) => a.name.localeCompare(b.name));
   if (sortBy === "nameZA")
-    sortedItems = menu.slice().sort((a, b) => b.name.localeCompare(a.name));
+    sortedItems = filteredItems
+      .slice()
+      .sort((a, b) => b.name.localeCompare(a.name));
   if (sortBy === "price01")
-    sortedItems = menu.slice().sort((a, b) => a.price - b.price);
+    sortedItems = filteredItems.slice().sort((a, b) => a.price - b.price);
   if (sortBy === "price10")
-    sortedItems = menu.slice().sort((a, b) => b.price - a.price);
+    sortedItems = filteredItems.slice().sort((a, b) => b.price - a.price);
 
   const currentItems = sortedItems.slice(startIndex, endIndex);
 
   return (
     <>
       <div>
+        <input
+          type="text"
+          placeholder="Search item..."
+          value={searchTerm}
+          onChange={(e) =>
+            dispatch({
+              type: ACTIONS.CHANGE_SEARCH_TERM,
+              payload: e.target.value,
+            })
+          }
+        />
+      </div>
+      <div className="text-white">
+        <button
+          onClick={() =>
+            dispatch({ type: ACTIONS.SET_FILTER, payload: "default" })
+          }
+        >
+          Whole menu
+        </button>
+        <button
+          onClick={() =>
+            dispatch({ type: ACTIONS.SET_FILTER, payload: "Coffee" })
+          }
+        >
+          Coffee
+        </button>
+        <button
+          onClick={() =>
+            dispatch({ type: ACTIONS.SET_FILTER, payload: "Desserts" })
+          }
+        >
+          Desserts
+        </button>
+        <button
+          onClick={() =>
+            dispatch({ type: ACTIONS.SET_FILTER, payload: "Specialty drinks" })
+          }
+        >
+          Secialty Drinks
+        </button>
+        <button
+          onClick={() => dispatch({ type: ACTIONS.SET_FILTER, payload: "Tea" })}
+        >
+          Tea
+        </button>
+        <button
+          onClick={() =>
+            dispatch({ type: ACTIONS.SET_FILTER, payload: "Breakfast" })
+          }
+        >
+          Breakfast
+        </button>
         <select
+          className="text-black"
           name="sortBy"
           value={sortBy}
           onChange={(e) =>
             dispatch({ type: ACTIONS.SELECT_SORT_BY, payload: e.target.value })
           }
         >
-          <option value="default">Default</option>
+          <option value="default">Default order</option>
           <option value="nameAZ">Name in ascending order</option>
           <option value="nameZA">Name in descending order</option>
           <option value="price01">The most cheap</option>
