869738e73dc6c28cebd15bacb8933affe6cf4a7d
Martin Beňa <Bena.M@seznam.cz>
2023-12-18 15:54:11 +0100

    Finalize responsivity of subhero

--- src/ui/Subhero.jsx
+++ src/ui/Subhero.jsx
@@ -7,7 +7,7 @@ function Subhero({ background, title, subtitle }) {
   return (
     <section
       ref={subheroRef}
-      className={`flex h-112 flex-col items-center justify-center ${background} bg-cover bg-center bg-no-repeat px-10 text-center dtxl:h-half-screen mob:px-5 [&>*:nth-child(1)]:mb-6 [&>*:nth-child(1)]:max-w-3xl [&>*:nth-child(1)]:xl:max-w-full`}
+      className={`flex h-112 flex-col items-center justify-center ${background} bg-cover bg-center bg-no-repeat px-10 text-center mob:px-5 [&>*:nth-child(1)]:mb-6 [&>*:nth-child(1)]:max-w-3xl [&>*:nth-child(1)]:xl:max-w-full`}
     >
       <HeadingPrimary>{title}</HeadingPrimary>
       <p className="text-xl font-medium text-white">{subtitle}</p>
