5d68d905e7f61b2f9515e3b05ba96bcfe150fd7c
Martin Beňa <Bena.M@seznam.cz>
2023-12-19 10:20:31 +0100

    Make the pagination responsive

--- src/features/menu/Pagination.jsx
+++ src/features/menu/Pagination.jsx
@@ -3,7 +3,7 @@ import PaginationNumbers from "./PaginationNumbers";
 
 function Pagination() {
   return (
-    <div className="mx-auto mt-20 flex max-w-sixty items-center justify-between">
+    <div className="mx-auto mt-20 flex max-w-sixty items-center justify-between tab:max-w-seventy moblg:max-w-full moblg:px-4">
       <PaginationButton direction="previous" />
       <PaginationNumbers />
       <PaginationButton direction="next" />
--- src/features/menu/PaginationNumbers.jsx
+++ src/features/menu/PaginationNumbers.jsx
@@ -4,24 +4,38 @@ import { useMenuContext } from "./MenuContext";
 function PaginationNumbers() {
   const { currentPage, maxPages, dispatch, ACTIONS } = useMenuContext();
   const allPages = getAvailablePages(maxPages);
+  const displayRange = [currentPage - 1, currentPage, currentPage + 1];
+
   return (
-    <div className="flex gap-4">
-      {allPages.map((page, i) => (
-        <button
-          className={`flex h-9 w-9 items-center justify-center rounded-full p-2 hover:bg-commontext hover:text-brandshade focus:outline-none focus:ring-2 focus:ring-commontext  ${
-            currentPage === i + 1
-              ? "bg-commontext text-brandshade focus:ring-offset-4 focus:ring-offset-brandshade"
-              : " focus:ring-offset-1 focus:ring-offset-commontext"
-          }`}
-          key={i}
-          onClick={() =>
-            dispatch({ type: ACTIONS.SET_CURRENT_PAGE, payload: page })
-          }
-          disabled={currentPage === i + 1}
-        >
-          {page}
-        </button>
-      ))}
+    <div className="flex flex-wrap justify-center gap-4">
+      {allPages.map((page, i) => {
+        const shouldDisplay = displayRange.includes(i + 1);
+
+        return (
+          <button
+            className={`flex h-9 w-9 items-center justify-center rounded-full p-2 hover:bg-commontext hover:text-brandshade focus:outline-none focus:ring-2 focus:ring-commontext ${
+              currentPage === i + 1
+                ? "bg-commontext text-brandshade focus:ring-offset-4 focus:ring-offset-brandshade"
+                : `focus:ring-offset-1 focus:ring-offset-commontext ${
+                    !shouldDisplay && "mob:hidden"
+                  }`
+            }`}
+            key={i}
+            onClick={() =>
+              dispatch({ type: ACTIONS.SET_CURRENT_PAGE, payload: page })
+            }
+            disabled={currentPage === i + 1}
+          >
+            <span className="hidden mob:inline">
+              {(i + 1 === displayRange[0] || i + 1 === displayRange[2]) &&
+                "..."}
+            </span>
+            <span className={`inline ${currentPage !== i + 1 && "mob:hidden"}`}>
+              {page}
+            </span>
+          </button>
+        );
+      })}
     </div>
   );
 }
