4f06ee18dd4c996a86be3a553ab9bb2ff34b01dd
Martin Beňa <Bena.M@seznam.cz>
2024-01-04 15:20:53 +0100

    Add the background to the team and reposition the h2

--- src/ui/Team.jsx
+++ src/ui/Team.jsx
@@ -33,20 +33,22 @@ const teamMembers = [
 
 function Team() {
   return (
-    <div className="mx-auto max-w-screen-2xl px-12 moblg:px-4 [&>*:nth-child(1)]:mb-12 [&>*:nth-child(1)]:text-center">
-      <HeadingSecondary>Our Team</HeadingSecondary>
-      <div className="flex justify-center gap-8 tab:flex-wrap tab:gap-y-12">
-        {teamMembers.map((member) => (
-          <TeamMember
-            key={member.fullName}
-            fullName={member.fullName}
-            position={member.position}
-            phone={member.phone}
-            email={member.email}
-            description={member.description}
-            image={member.image}
-          />
-        ))}
+    <div className="mx-auto max-w-screen-2xl px-12 tablg:px-4 moblg:px-4">
+      <div className="rounded-lg bg-brandtint p-8 mob:px-4 [&>*:nth-child(1)]:mx-auto [&>*:nth-child(1)]:mb-12 [&>*:nth-child(1)]:max-w-max [&>*:nth-child(1)]:border-b-2 [&>*:nth-child(1)]:border-commontext [&>*:nth-child(1)]:pb-2">
+        <HeadingSecondary>Our Team</HeadingSecondary>
+        <div className="flex justify-center gap-8 tab:flex-wrap tab:gap-y-12">
+          {teamMembers.map((member) => (
+            <TeamMember
+              key={member.fullName}
+              fullName={member.fullName}
+              position={member.position}
+              phone={member.phone}
+              email={member.email}
+              description={member.description}
+              image={member.image}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
