40867d38bf1474a2a5dbcc11e572c31544653f22
Martin Beňa <Bena.M@seznam.cz>
2024-01-08 13:00:43 +0100

    Set the height of Wave based on the viewport

--- src/ui/Wave.jsx
+++ src/ui/Wave.jsx
@@ -7,9 +7,11 @@ const path2 =
   "M0 226L80 226.5C160 227 320 228 480 215.3C640 202.7 800 176.3 960 167.2C1120 158 1280 166 1360 170L1440 174V0H1360C1280 0 1120 0 960 0C800 0 640 0 480 0C320 0 160 0 80 0H0V226Z";
 
 function Wave() {
+  const height = window.innerWidth <= 1344 ? 240 : 160;
+
   return (
     <svg
-      viewBox="0 110 1440 240"
+      viewBox={`0 110 1440 ${height}`}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
