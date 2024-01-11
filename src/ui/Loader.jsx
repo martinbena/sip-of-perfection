a175cb3e5497376b6f6a5a48fbb2043147f64c09
Martin Beňa <Bena.M@seznam.cz>
2024-01-11 22:00:31 +0100

    Make the Loader component to be in fixed position instead of absolute

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,6 +1,6 @@
 function Loader() {
   return (
-    <div className="absolute left-0 top-0 z-50 flex h-screen w-full items-center justify-center bg-brandshade">
+    <div className="fixed left-0 top-0 z-50 flex h-screen w-full items-center justify-center bg-brandshade">
       <div className="loader">&nbsp;</div>
     </div>
   );
