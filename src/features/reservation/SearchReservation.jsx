a893cadd663e2a38e1e1d831972477127ccc25e2
Martin Beňa <Bena.M@seznam.cz>
2024-01-12 10:49:37 +0100

    Make the reservation searchbar responsive

--- src/features/reservation/SearchReservation.jsx
+++ src/features/reservation/SearchReservation.jsx
@@ -13,11 +13,22 @@ function SearchReservation() {
     setQuery("");
   }
 
+  // screens: {
+  //       dtxl: { min: "2540px" },
+  //       dt: { max: "1540px" },
+  //       dtsm: { max: "1344px" },
+  //       tablg: { max: "1200px" },
+  //       tab: { max: "944px" },
+  //       moblg: { max: "704px" },
+  //       mob: { max: "544px" },
+  //       mobsm: { max: "440px" },
+  //     },
+
   return (
-    <div className="mx-auto max-w-8xl px-24">
-      <form className="child:ml-auto" onSubmit={handleSubmit}>
+    <div className="mx-auto max-w-8xl px-16 mob:px-2">
+      <form className="child:ml-auto mob:child:mx-auto" onSubmit={handleSubmit}>
         <Searchbar
-          placeholder="Search reservation..."
+          placeholder="Search reservation ID..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
