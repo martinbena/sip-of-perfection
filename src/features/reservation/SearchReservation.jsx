08ac8e852c992a47b7d0c8d27368bd2838524280
Martin Beňa <Bena.M@seznam.cz>
2024-01-18 20:04:44 +0100

    Create a validation for SearchReservation component

--- src/features/reservation/SearchReservation.jsx
+++ src/features/reservation/SearchReservation.jsx
@@ -1,30 +1,44 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Searchbar from "../../ui/Searchbar";
+import ReservationStatus from "../../ui/ReservationStatus";
+import { PiWarning } from "react-icons/pi";
 
 function SearchReservation() {
   const [query, setQuery] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
   const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
-    if (!query) return;
+    setIsTouched(true);
+    if (query.length !== 20) return;
     navigate(`/reservation/${query}`);
     setQuery("");
   }
 
   return (
     <div className="mx-auto max-w-8xl px-16 mob:px-2">
-      <form
-        className="child:ml-auto mob:child:mx-auto"
-        aria-label="Search your reservation"
-        onSubmit={handleSubmit}
-      >
-        <Searchbar
-          placeholder="Search reservation ID..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
+      <form aria-label="Search your reservation" onSubmit={handleSubmit}>
+        <div
+          className={`ml-auto w-96 ${
+            isTouched && query !== 20 ? "flex flex-col gap-2" : ""
+          }`}
+        >
+          <Searchbar
+            placeholder="Search reservation ID..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            inputIsInvalid={isTouched && query.length !== 20}
+          />
+          {isTouched && query.length !== 20 && (
+            <ReservationStatus
+              type="danger"
+              status="Please insert a valid ID."
+              icon={<PiWarning />}
+            />
+          )}
+        </div>
       </form>
     </div>
   );
--- src/ui/Searchbar.jsx
+++ src/ui/Searchbar.jsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import { GiMagnifyingGlass } from "react-icons/gi";
 
-function Searchbar({ placeholder, value, onChange }) {
+function Searchbar({ placeholder, value, onChange, inputIsInvalid }) {
   const inputRef = useRef(null);
 
   function focusInput() {
@@ -10,14 +10,20 @@ function Searchbar({ placeholder, value, onChange }) {
   return (
     <div
       onClick={focusInput}
-      className="searchbar flex w-96 items-center gap-8 rounded-full bg-white px-6 py-3 shadow-menu-inputs focus-within:outline focus-within:outline-2 focus-within:outline-commontext mob:py-1.5 mobsm:w-full"
+      className={`flex w-96 items-center gap-8 rounded-full  px-6 py-3 shadow-menu-inputs  mob:py-1.5 mobsm:w-full ${
+        inputIsInvalid
+          ? "bg-red-100 outline outline-2 outline-red-700"
+          : "bg-white focus-within:outline focus-within:outline-2 focus-within:outline-commontext"
+      }`}
     >
       <GiMagnifyingGlass className="h-8 w-8" />
       <input
         ref={inputRef}
         type="text"
         placeholder={placeholder}
-        className="w-full focus:outline-none"
+        className={`w-full focus:outline-none ${
+          inputIsInvalid ? "bg-red-100 text-red-700" : ""
+        }`}
         value={value}
         onChange={onChange}
       />
