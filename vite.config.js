7a5a0afb7890fddae598961082371ba14688e14d
Martin Beňa <Bena.M@seznam.cz>
2024-01-18 15:38:38 +0100

    Configure Vite to execute even more chunking

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,19 @@ export default defineConfig({
       promiseImportName: (i) => `__tla_${i}`,
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
