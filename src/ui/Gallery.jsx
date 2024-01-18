c3489f7a9b213df71f8ec51d795a0f752cb2f4b2
Martin Beňa <Bena.M@seznam.cz>
2024-01-18 09:23:08 +0100

    Move the data for testimonials, team members and gallery from arrays within the components into the json files within the data folder

--- src/ui/Gallery.jsx
+++ src/ui/Gallery.jsx
@@ -1,32 +1,7 @@
 import GalleryItem from "./GalleryItem";
+import pictures from "../data/galleryData.json";
 
 function Gallery() {
-  const pictures = [
-    {
-      title: "Coffee machine",
-      src: "src/assets/gallery-coffee-machine.jpg",
-    },
-    {
-      title: "Coffee cake",
-      src: "src/assets/gallery-cake.jpg",
-    },
-    {
-      title: "Coffee with croissant",
-      src: "src/assets/gallery-coffee-croissant.jpg",
-    },
-    {
-      title: "Barista",
-      src: "src/assets/gallery-barista.jpg",
-    },
-    {
-      title: "Tiramisu",
-      src: "src/assets/gallery-tiramisu.jpg",
-    },
-    {
-      title: "Coffee pot",
-      src: "src/assets/gallery-coffee-pot.jpg",
-    },
-  ];
   return (
     <div className="grid grid-cols-2 items-center gap-12 py-12 pr-20 dtsm:gap-6 dtsm:pr-12 tablg:grid-cols-3 tablg:p-12 tab:grid-cols-2 tab:px-8 mob:grid-cols-1">
       {pictures.map((pic) => (
--- src/ui/Team.jsx
+++ src/ui/Team.jsx
@@ -1,35 +1,6 @@
 import HeadingSecondary from "./HeadingSecondary";
 import TeamMember from "./TeamMember";
-
-const teamMembers = [
-  {
-    fullName: "Jordan Chamberlain",
-    position: "owner",
-    phone: "555-987-6543",
-    email: "jordan@sop.com",
-    description:
-      "The creative force behind Sip of Perfection, devoted to crafting a warm and memorable atmosphere for all.",
-    image: "/src/assets/jordan-chamberlain.jpg",
-  },
-  {
-    fullName: "Sarah Mitchell",
-    position: "manager",
-    phone: "555-876-5432",
-    email: "sarah@sop.com",
-    description:
-      "Guiding our seamless operations, our dedicated manager upholds the high standards of hospitality at Sip of Perfection.",
-    image: "/src/assets/sarah-mitchell.webp",
-  },
-  {
-    fullName: "Lorenzo DiMatteo",
-    position: "chef",
-    phone: "555-765-4321",
-    email: "lorenzo@sop.com",
-    description:
-      "Indulge in culinary excellence, where every dish reflects the chef's commitment to a memorable dining experience at Sip of Perfection.",
-    image: "/src/assets/lorenzo-dimatteo.webp",
-  },
-];
+import teamMembers from "../data/teamMembers.json";
 
 function Team() {
   return (
