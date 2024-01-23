a40054ae6e926460c4cb900ec685a38aaabeefd9
Martin Beňa <Bena.M@seznam.cz>
2024-01-23 10:45:47 +0100

    Implement middleware to redux global UI state, so the pre-ordered items are saved in the local storage

--- src/store.js
+++ src/store.js
@@ -1,12 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./features/cart/cartSlice";
+import cartReducer, { loadCart } from "./features/cart/cartSlice";
 import navigationReducer from "./navigationSlice";
+import reservationReducer from "./features/reservation/reservationSlice";
+
+const localStorageMiddleware = (store) => (next) => (action) => {
+  const result = next(action);
+
+  if (
+    action.type === "cart/addItem" ||
+    action.type === "cart/deleteItem" ||
+    action.type === "cart/increaseItemQuantity" ||
+    action.type === "cart/decreaseItemQuantity" ||
+    action.type === "cart/clearCart"
+  ) {
+    localStorage.setItem("cart", JSON.stringify(store.getState().cart.cart));
+  }
+
+  return result;
+};
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
     navigation: navigationReducer,
+    reservation: reservationReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
 });
+
+const savedCart = localStorage.getItem("cart");
+if (savedCart) {
+  store.dispatch(loadCart(JSON.parse(savedCart)));
+}
 
 export default store;
